48e17d5134a4b7907feaf46f7946ec97a72a5a06
Frank Nitschke <frank@mail-nitschke.de>
2023-05-31 22:31:17 +0200

    add update route

--- server/routes/update.js
+++ server/routes/update.js
@@ -2,12 +2,21 @@ const express = require('express');
 const router = express.Router();
 const simpleGit = require('simple-git');
 
-const { execFile, spawn } = require('node:child_process');
+const path = require('path');
+
+const { exec, execFile, spawn } = require('node:child_process');
 
 const { middleware } = require('../helper');
+//const { error } = require('node:console');
+//const { stdout, stderr } = require('node:process');
 
 //protect routes
 //router.use(middleware);
+
+router.use((req, res, next) => {
+  res.append('content-type', 'application/json');
+  next();
+});
 
 const options = {
   baseDir: process.cwd(),
@@ -27,28 +36,73 @@ async function gitStatus() {
   }
 }
 
+async function gitPull() {
+  try {
+    return await git.pull();
+  } catch (error) {
+    console.error(error);
+    return error;
+  }
+}
+
 router.get('/status', async (req, res) => {
   const status = await gitStatus();
-  console.log(new Date());
-  /* const child = execFile('npm', ['install'], (error, stdout, stderr) => {
-    if (error) {
-      throw error;
-    }
-    console.log(stdout);
-    console.log(stderr);
-  }); */
-  spawn('npm', ['install'], {
-    cwd: __dirname, // <---
-    shell: true,
-    stdio: 'inherit',
-  });
 
-  //console.log(npm);
+  if (status.behind === 0) {
+    return res.status(200).send({
+      msg: 'Keine Updates Verfügbar',
+      v: process.env.npm_package_version,
+      link: 'https://github.com/franknitschke/referee/releases',
+    });
+  } else {
+    return res.status(200).send({
+      msg: 'Neues Updates Verfügbar',
+      v: process.env.npm_package_version,
+      link: 'https://github.com/franknitschke/referee/releases',
+    });
+  }
+
+  //GEHT
+  /* exec('npm i', (error, stdout, stderr) => {
+    if (error) console.log(`Error: ${error}`);
+    if (stdout) console.log(`Konsole: ${stdout}`);
+    if (stderr) console.log(`Error: ${stderr}`);
+  }); */
 
   //console.log(JSON.stringify(process.env.npm_package_version));
 
-  res.header({ 'content-type': 'application/json' });
-  res.status(200).send(JSON.stringify(status, null, 4));
+  //GEHT
+  /* exec(path.join(__dirname, '..', 'script.sh'), function (err, stdout, stderr) {
+    if (err) console.log(`Error: `, err);
+    if (stdout) console.log(`Konsole: `, stdout);
+    if (stderr) console.log(`Error: `, stderr);
+  }); */
+
+  //res.status(200).send(JSON.stringify(status.behind, null, 4));
+});
+
+router.post('/', async (req, res) => {
+  const status = await gitPull();
+  console.log('Git Pull: ', status);
+
+  if (status) {
+    exec('npm i', (error, stdout, stderr) => {
+      if (error) {
+        console.log(`Error: ${error}`);
+        return res.status(500).send({ msg: 'Es ist ein Fehler aufgetreten!' });
+      }
+      if (stdout) {
+        console.log(`Konsole: ${stdout}`);
+        return res.status(200).send({ msg: 'Update erfolgreich!' });
+      }
+      if (stderr) {
+        console.log(`Error: ${stderr}`);
+        return res.status(500).send({ msg: 'Es ist ein Fehler aufgetreten!' });
+      }
+    });
+  }
+
+  res.status(500).send({ msg: 'Es ist ein Fehler aufgetreten' });
 });
 
 module.exports = router;

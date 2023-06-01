ee230ac7792427a2e51abd855ec2f240b68c3315
Frank Nitschke <frank@mail-nitschke.de>
2023-06-01 21:44:42 +0200

    fe update settings

--- client/src/components/SettingsUpdateCard.jsx
+++ client/src/components/SettingsUpdateCard.jsx
@@ -5,6 +5,7 @@ import Loading from './Loading';
 
 function SettingsUpdateCard({ accessToken }) {
   const [updates, setUpdates] = useState(null);
+  const [updateMsg, setUpdateMsg] = useState(null);
 
   const { loading, error, success, fetchData } = useSubmit();
 
@@ -13,9 +14,14 @@ function SettingsUpdateCard({ accessToken }) {
     setUpdates(data);
   }
 
+  async function handleSubmit() {
+    const update = await fetchData('/api/update', 'POST', null, accessToken);
+    if (update) setUpdateMsg(update);
+  }
+
   useEffect(() => {
     getUpdateData();
-  }, []);
+  }, [updateMsg]);
   return (
     <>
       {!updates ? (
@@ -38,20 +44,42 @@ function SettingsUpdateCard({ accessToken }) {
               Versionsbeschreibung ansehen
             </a>
           </div>
-          <div className='py-4 font-semibold text-lg'>{`Die Durchführung des Updates kann einige Zeit dauern.
+          {!updates.flag && (
+            <>
+              <div className='py-4 font-semibold text-lg'>{`Die Durchführung des Updates kann einige Zeit dauern.
           Nach dem Update müssen Sie den PI neu starten!`}</div>
-          <div>
-            <button
-              tabIndex={0}
-              disabled={loading ? true : false}
-              type='submit'
-              className={`btn btn-outline ${error && 'btn-error'} ${
-                success && 'btn-success'
-              } ${loading && 'loading'}`}
-            >
-              {error ? 'Fehler' : 'Update'}
-            </button>
-          </div>
+              <div>
+                <button
+                  onClick={handleSubmit}
+                  disabled={loading ? true : false}
+                  type='submit'
+                  className={`btn btn-outline ${error && 'btn-error'} ${
+                    success && 'btn-success'
+                  } ${loading && 'loading'}`}
+                >
+                  {error ? 'Fehler' : 'Update'}
+                </button>
+              </div>
+            </>
+          )}
+          {updateMsg && (
+            <div class='alert alert-success my-4'>
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                class='stroke-current shrink-0 h-6 w-6'
+                fill='none'
+                viewBox='0 0 24 24'
+              >
+                <path
+                  stroke-linecap='round'
+                  stroke-linejoin='round'
+                  stroke-width='2'
+                  d='M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z'
+                />
+              </svg>
+              <span>{updateMsg.msg}</span>
+            </div>
+          )}
         </>
       )}
       {/*  {JSON.stringify(updates)} */}

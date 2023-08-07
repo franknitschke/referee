00a73b52a18bfb3342a6b7d0f38bdce2d5b7974e
Frank Nitschke <frank@mail-nitschke.de>
2023-08-07 19:00:47 +0200

    delete Test Page

--- client/src/pages/Test.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import OnlineStatus from '../components/OnlineStatus';
-
-function Test({ competitionData }) {
-  return <div><OnlineStatus /></div>;
-}
-
-export default Test;
